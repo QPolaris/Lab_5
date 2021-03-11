013b3a381f1e42471a69c86f78dfee09d87674e4
Autograde Server <autograde@autograde.local>
2021-03-11 02:08:59 +0000

    Initial Starter Generated

---
+++ p0.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+from random import randrange
+
+
+def RandomizedPartition(array, low, high):
+    """
+    Implement Randomized partitioning from Cormen book (same as lab 4)
+    """
+
+    return -1
+    
+
+
+def OrderStatistics(array, p, r, i):
+    """
+    Return ith order statistics
+    OrderStatistics(array, p, r, i), returns  the ith smallest element of the array array[p...r]
+    
+    For example in array = [81,71,99,68,45,55,0,22,11,34]
+    return 0 for i = 0 [smallest]
+    return 11 for i = 1
+    return 22 for i = 2
+    return 34 for i = 3
+    return 45 for i = 4
+    return 55 for i = 5
+    return 68 for i = 6
+    return 71 for i = 7
+    return 81 for i = 8
+    return 99 for i = 9 [largest]
+    
+    Your solution should run in lineartime using RandomizedPartition.
+    40% points will be deducted for non-linear solutions
+    
+    For hints check Chapter 9 from Cormen book.
+    
+    """
+    return -1
